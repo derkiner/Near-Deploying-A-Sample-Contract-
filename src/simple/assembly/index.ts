df4038334e0a11645e6b1bef900bccb2ea9a77a5
phoenixdamla <90147636+derkiner@users.noreply.github.com>
2022-04-16 12:39:34 +0300

    Update index.ts

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -2,7 +2,7 @@ import { storage, Context } from "near-sdk-as"
 
 // return the string 'hello world'
 export function helloWorld(): string {
-  return 'hello world'
+  return 'hello Damla'
 }
 
 // read the given key from account (contract) storage
